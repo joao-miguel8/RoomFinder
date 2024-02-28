8351a96f736cd3c3d01c3cede902514e4c0a7db1
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-02-28 14:04:26 -0500

    - Add custom tailwind styles
    - Change file extension for postcss.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,9 +13,25 @@ export default {
 	content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
 	theme: {
 		fontSize: fontSizes,
-		extend: {},
+		extend: {
+			colors: {
+				primary01: "#0FC194",
+				primary02: "#24C99C",
+				primary03: "#00B084",
+				primary04: "#009A70",
+				light01: "#F9F9F9",
+				dark01: "#21247",
+				gray200: "#F9F9F9",
+			},
+		},
 		fontFamily: {
 			montserrat: "montserrat",
+		},
+		borderRadius: {
+			b2: "8px",
+		},
+		boxShadow: {
+			bs1: "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px",
 		},
 	},
 	plugins: [],
