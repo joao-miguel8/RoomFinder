4f79ba7bdadaf971c120f7deb77d9f18a7c941d1
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-03-04 13:38:10 -0500

    - Add and setup MongoDB for backend
    - Add dotenv, body-parser, express, mongoose npm packages
    - Add .env file to .gitignore
    - Add MongoDB database connection to index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,23 @@
 import http from "http";
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+// Load environment variables from .env file
+dotenv.config();
+
+// Express Connection
+export const app = express();
+
+// Database Connection
+mongoose.connect(process.env.MONGODB_URI as string);
+export const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("MongoDB Connected");
+});
+
+// Node Server Connection
 export const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-type": "application/json" });
   res.end(JSON.stringify({ data: "Server started!" }));
